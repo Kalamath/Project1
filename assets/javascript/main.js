8ae40f6b58b186fefd1eef6ae35cbef40eb1690d
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-27 16:37:51 -0700

    working AJAX calls/results div for USHER

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,28 +13,20 @@ function resultsDiv() {
     var artists = [
         {   name: "Usher",
             
-            queryURL: "https://api.data.charitynavigator.org/v2/Organizations?app_id=270bf11f&app_key=6fbc2df180aae26a94dfe40a27140c98&pageSize=5&pageNum=5&search=Boys%20and%20Girls%20Club%20of%20America&searchType=NAME_ONLY&rated=true",
-            // img: url("..images/usher.jpg"),
-    
+            queryURL: "https://api.data.charitynavigator.org/v2/Organizations?app_id=270bf11f&app_key=6fbc2df180aae26a94dfe40a27140c98&pageSize=1&pageNum=1&search=Boys%20and%20Girls%20Club%20of%20America&searchType=NAME_ONLY&rated=true",
+            img: "./assets/images/usher.jpg",
           },
 
         {   name: "Jennifer Lopez",
-    
-    
-    
+            queryURL: "",
         },
 
         {   name: "Beyoncè",
-    
-    
-    
+            queryURL: "",
         },
         {   name: "Elton John",
-    
-    
-    
+            queryURL: "",    
         },
-
 
     ]
 
@@ -52,6 +44,7 @@ function resultsDiv() {
               var newButton = $("<button>");
               newButton.addClass("celebBtn");
               newButton.attr("data-name", artists[i].name);
+              newButton.attr("query-link", artists[i].queryURL)
               newButton.text(artists[i].name);
               $("#artist").append(newButton);
         }
@@ -61,26 +54,36 @@ function resultsDiv() {
     getArtists();
 
 
-//API CALLS FOR CELEBRITY BUTTONS
 
-$(".celebBtn").on("click", function() {
 
-    $(".searchResultsDiv").show();
+$(".celebBtn").on("click", function(results) {
+      event.preventDefault();
+     $("#resultsText").empty();  
+     $(".searchResultsDiv").show();
+    
+    // Creates Images from Celeb Object and Appends to Search Results Div
+    var img = $("<img>");
+    img.addClass("searchResultPhotos");
+    img.attr("src", this, artists.img);
+    var queryURL= $(this).attr("query-link");
+    $("#celebphoto").append(img)
+    console.log(img, "image")
 
-    var queryURL= this.queryURL;
-
-    $.ajax({
+ //API CALLS FOR CELEBRITY BUTTONS   
+   $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-      var results = response.data
-        var chartName = results.charityName;
-        var tagline = results.tagLine;
-        var cause = results.cause.caseName;
 
-        console.log(chartyName)
-        $("#resultsText").text(chartsName + tagline+ cause)
-        $("#celebphot").append(this.artists.photo)
+    console.log (response);
+        var chartName = response[0].charityName;
+        var tagline = response[0].tagLine;
+        var purpose = response[0].cause.causeName;
+        var mission= response[0].mission;
+        var site = response[0].websiteURL;
+        $("#resultsText").append("Charity Name: " + chartName +"<br>"+ "Charity Tagline: " + tagline + 
+        "<br>" + "Charity Purpose: " + purpose + "<br>" + "Mission Statement: " + mission + "<br>" + "Get Involved: " + site);
+     
     });
 
     });
