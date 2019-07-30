589a9a455e829ccc70b6a706d5c197139c7179a0
Ashton Roth <ashtonjroth@gmail.com>
2019-07-29 21:47:38 -0700

    got map to take user location

--- assets/javascript/google.js
+++ assets/javascript/google.js
@@ -1,35 +1,56 @@
 $(document).ready(function(){
   initAutocomplete()
   function initAutocomplete() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        console.log(position)
-        var latitude = position.coords.latitude
-        var longitude = position.coords.longitude
 
-        console.log('should see first');
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: latitude, lng: longitude},
-          zoom: 13,
-          mapTypeId: 'roadmap'
-        });
-      
-        var request = {
-          query: 'Charities',
-          fields: ['name', 'geometry'],
-        };
     
-       var service=new google.maps.places.PlacesService(map);
-       service.findPlaceFromQuery(request, function(results, status) {
     
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          console.log(results);
-        }})
+
+
+    getCoords().then(function(data){
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: data.latitude, lng: data.longitude},
+        zoom: 13,
+        mapTypeId: 'roadmap'
       });
-    }
-    console.log('should see second');
+    
+      var request = {
+        query: 'kfc',
+        fields: ['name', 'geometry'],
+      };
+  
+     var service=new google.maps.places.PlacesService(map);
+     service.findPlaceFromQuery(request, function(results, status) {
+  
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+      }})  
+      console.log(data); 
+    })
  
+  }
 
-}
+  function getCoords(){
+    return new Promise(function (resolve , reject ){
+
+      if (navigator.geolocation) {
+      
+        navigator.geolocation.getCurrentPosition(function(position){
+          console.log(position)
+          var latitude = position.coords.latitude
+          var longitude = position.coords.longitude
+
+          resolve({
+            latitude: latitude,
+            longitude: longitude, 
+          })
+  
+        });
+      } else {
+        reject("does not work")
+      }
+
+    })
+  }
+
 });
  
