06920c3c6d6299e1c0ee5f9eacad3f7e692d48ac
Ashton Roth <ashtonjroth@gmail.com>
2019-07-31 19:05:38 -0700

    drop user input pin

--- assets/javascript/google.js
+++ assets/javascript/google.js
@@ -2,30 +2,77 @@ $(document).ready(function(){
   initAutocomplete()
   function initAutocomplete() {
 
-    
-    
-
-
     getCoords().then(function(data){
+      
       var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: data.latitude, lng: data.longitude},
         zoom: 13,
         mapTypeId: 'roadmap'
       });
+      var marker = new google.maps.Marker({
+        position: {lat: data.latitude, lng: data.longitude},
+        map: map,
+        title: 'Hello World!'
+      });
+     locationRequest(data.latitude, data.longitude).then(function(results){
+       console.log(results)
+
+      //  var charityLocation = results
+      //  var marker = new google.maps.Marker({
+      //   position: myLatLng,
+      //   map: map,
+      //   title: 'Hello World!'
+      // });
+     });
+      // var request = {
+      //   query: 'school',
+      //   fields: ['name', 'geometry'],
+      // };
+
+      // var service = new google.maps.places.PlacesService(map);
+
+      // service.findPlaceFromQuery(request, function(results, status) {
+      //   console.log(results);
+      //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+      //     for (var i = 0; i < results.length; i++) {
+      //       createMarker(results[i]);
+      //     }
+
+      //     map.setCenter(results[0].geometry.location);
+      //   }
+      // });
+
+    });
+
+  }
+
+    function createMarker(place) {
+      
+    //   var marker = new google.maps.Marker({
+    //     map: map,
+    //     position: place.geometry.location
+    //   });
+
+    //   google.maps.event.addListener(marker, 'click', function() {
+    //     infowindow.setContent(place.name);
+    //     infowindow.open(map, this);
+    //   });
+
+    //  var service=new google.maps.places.PlacesService(map);
+    //  service.findPlaceFromQuery(request, function(results, status) {
+  
+    //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //     console.log(results);
+        
+    //   }
+      
+    // })  
+
+    //   console.log(data); 
+      
+    // })
+
     
-      var request = {
-        query: 'kfc',
-        fields: ['name', 'geometry'],
-      };
-  
-     var service=new google.maps.places.PlacesService(map);
-     service.findPlaceFromQuery(request, function(results, status) {
-  
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results);
-      }})  
-      console.log(data); 
-    })
  
   }
 
@@ -48,9 +95,19 @@ $(document).ready(function(){
       } else {
         reject("does not work")
       }
-
     })
+      
   }
 
-});
- 
+  function locationRequest(lat,lng) {
+    
+    return $.ajax({
+      type: "GET",
+      url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=charities&location="+ lat +","+ lng +"&key=AIzaSyCfCbQMtphVTJZPsHwJfmHVYIgcWCpITKk",
+    });
+
+  }
+
+
+}); 
+
