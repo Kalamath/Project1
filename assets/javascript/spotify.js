473a9af43bfee8ef06fa76186901971c4e29e2c9
Kalamath <thecodedimage@gmail.com>
2019-07-31 20:21:51 -0700

    spotify biz

--- assets/javascript/spotify.js
+++ assets/javascript/spotify.js
@@ -1,29 +1,18 @@
-window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQAeNl9ov5XnMFMFu7bzPb9BOBzIShpi8YfwXHZvT7l0kvVIkWk1YpVo2H4KJcU5PGWlPVs6dk0E2dUf1CfaEZ41Y406gjdlwoF9tkC2qbAa9U9Jp2Dp8iRLOpswDZ1sFzc7VrFg9Z-mdzb5plfXTY_zNBn3t4QoFcQ6HvY';
-    const player = new Spotify.Player({
-        name: 'Web Playback SDK Quick Start Player',
-        getOAuthToken: cb => { cb(token); }
-    });
 
-    // Error handling
-    player.addListener('initialization_error', ({ message }) => { console.error(message); });
-    player.addListener('authentication_error', ({ message }) => { console.error(message); });
-    player.addListener('account_error', ({ message }) => { console.error(message); });
-    player.addListener('playback_error', ({ message }) => { console.error(message); });
+function playSong ( ) {
+    var accessToken = localStorage.getItem("token");
 
-    // Playback status updates
-    player.addListener('player_state_changed', state => { console.log(state); });
+    var data = {
+        uris: ["spotify:track:7oqftogUN82Q7VNy2TmTJW"]
+    }
 
-    // Ready
-    player.addListener('ready', ({ device_id }) => {
-        console.log('Ready with Device ID', device_id);
-    });
-
-    // Not Ready
-    player.addListener('not_ready', ({ device_id }) => {
-        console.log('Device ID has gone offline', device_id);
-    });
-
-    // Connect to the player!
-    player.connect();
-};
+    $.ajax({
+        method: "PUT",
+        url: " https://api.spotify.com/v1/me/player/play",
+        headers: {
+            "Authorization": "Bearer " + accessToken 
+        }, 
+        data: JSON.stringify(data),
+        contentType: "application/json"
+    })
+}
