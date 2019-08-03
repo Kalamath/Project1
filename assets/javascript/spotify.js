a174bbdf86ea0e55685997144dde1d7437518905
Kalamath <thecodedimage@gmail.com>
2019-08-02 20:00:03 -0700

    played with some api stuff

--- assets/javascript/spotify.js
+++ assets/javascript/spotify.js
@@ -1,18 +1,18 @@
-function playSong () {
-    
+function playSong() {
+
     var accessToken = localStorage.getItem("token");
-    
+
     var data = {
-        uris: ["spotify:track:7oqftogUN82Q7VNy2TmTJW"]
-        
+        uris: ["spotify:track:48RN2EOOyG2Gs5Pyla7ZJj", "spotify:track:7pu8AhGUxHZSCWTkQ2eb5M", "spotify:track:1yo16b3u0lptm6Cs7lx4AD", "spotify:track:1yo16b3u0lptm6Cs7lx4AD"]
+
     }
-    
+
     $.ajax({
         method: "PUT",
         url: " https://api.spotify.com/v1/me/player/play",
         headers: {
-            "Authorization": "Bearer " + accessToken 
-        }, 
+            "Authorization": "Bearer " + accessToken
+        },
         data: JSON.stringify(data),
         contentType: "application/json"
     })
